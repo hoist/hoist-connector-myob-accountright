61e9ce336ae10d63292a5e6af48ee01f37200b79
Owen Evans <owen@bgeek.net>
2015-09-16 15:47:43 +1200

    use base box

--- deploy.sh
+++ deploy.sh
@@ -6,22 +6,22 @@ TIMESTAMP=$(date +%Y%m%d_%H%M%S)
 
 echo "making timestamped deploy directory";
 
-mkdir -p ${ROOT_CONNECTOR_DIR}/hoist-connector-myob-accountright/${TIMESTAMP}
+mkdir -p ${ROOT_CONNECTOR_DIR}/hoist-connector-${CONNECTOR_NAME}/${TIMESTAMP}
 
 echo "copying files to deploy directory";
 
-cp -r . ${ROOT_CONNECTOR_DIR}/hoist-connector-myob-accountright/${TIMESTAMP}
+cp -r . ${ROOT_CONNECTOR_DIR}/hoist-connector-${CONNECTOR_NAME}/${TIMESTAMP}
 
 echo "removing current symlink";
 
-rm -f ${ROOT_CONNECTOR_DIR}/hoist-connector-myob-accountright/current
+rm -f ${ROOT_CONNECTOR_DIR}/hoist-connector-${CONNECTOR_NAME}/current
 
 echo "relinking current symlink";
 
-ln -s ${ROOT_CONNECTOR_DIR}/hoist-connector-myob-accountright/${TIMESTAMP} ${ROOT_CONNECTOR_DIR}/hoist-connector-myob-accountright/current
+ln -s ${ROOT_CONNECTOR_DIR}/hoist-connector-${CONNECTOR_NAME}/${TIMESTAMP} ${ROOT_CONNECTOR_DIR}/hoist-connector-${CONNECTOR_NAME}/current
 
 echo "removing old deploy directories";
 
-(ls -t ${ROOT_CONNECTOR_DIR}/hoist-connector-myob-accountright/|head -n 5;ls ${ROOT_CONNECTOR_DIR}/hoist-connector-myob-accountright/)|sort|uniq -u|xargs -I '{}' rm -r ${ROOT_CONNECTOR_DIR}/hoist-connector-myob-accountright/'{}'
+(ls -t ${ROOT_CONNECTOR_DIR}/hoist-connector-${CONNECTOR_NAME}/|head -n 5;ls ${ROOT_CONNECTOR_DIR}/hoist-connector-${CONNECTOR_NAME}/)|sort|uniq -u|xargs -I '{}' rm -r ${ROOT_CONNECTOR_DIR}/hoist-connector-${CONNECTOR_NAME}/'{}'
 
 echo "done!";
