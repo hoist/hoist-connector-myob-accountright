365d7acc411d0edf9dac7b3f594d94b537ef590f
Jamie Wilson <jamie@hoist.io>
2016-04-03 22:22:12 +1200

    view update

--- src/views/edit.js
+++ src/views/edit.js
@@ -19,22 +19,23 @@ class EditForm extends C.View {
     return (
       <C.Page default="setup" {...this.props}>
         <C.Panel name="Setup" slug="setup">
-          <UI.FormElements.Button text={this.props.connectorInstance ? 'Reauthorize' : 'Connect'} type="large" onClick={()=>{
-              return this.connect();
-            }} />
+          <form onChange={(evt) => {
+            this.props.updateField(evt);
+          }} onSubmit={(evt) => {
+            this.props.updateSettings(evt);
+          }}>
+            <UI.FormElements.Input placeholder="Key" name="key" label="Key" type="text" value={this.props._key}/>
+            <UI.FormElements.Input placeholder="App Id" name="clientId" label="App Id" type="text" value={this.props.settings.clientId}/>
+            <UI.FormElements.Input placeholder="API Key" name="clientSecret" label="API Key" type="text" value={this.props.settings.clientSecret}/>
+            <UI.FormElements.Button
+              loading={this.props.saving}
+              text={this.props.connectorInstance ? 'Save' : 'Create'}
+              type="large"
+              submit={true}
+              onClick={this.props.updateSettings} />
+          </form>
         </C.Panel>
-        {this.props.connectorInstance ? <C.Panel name="Events" slug="events">
-        <C.PageHeader
-          title="Check the boxes of the events you want to subscribe to."
-          subTitle="Checking a box will automatically subscribe you to that event." />
-          <C.CheckboxGrid
-            items={this.getAvailableEvents()}
-            checked={this.getSubscribedEvents()}
-            onChange={this.props.onSubscribe} />
-        </C.Panel> : <C.Panel name="Events" slug="events">
-          <C.EventsGrid.Header
-            title="Events are available once you've connected." />
-        </C.Panel>}
+        <C.Panel name="instructions" slug="Instructions"></C.Panel>
       </C.Page>
     );
   }
